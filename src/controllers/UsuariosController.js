d8fa83a9d94ca1950dd4235f96594907879b4763
Leonardo da Costa da Silva <leonardo.costa.silva@g.globo>
2023-09-04 09:03:06 -0300

    doc: inclusão de comentários ao controller Usuários

--- src/controllers/UsuariosController.js
+++ src/controllers/UsuariosController.js
@@ -1,9 +1,19 @@
 class UsuariosController{
+    /**
+     * Método para contralização de rotas no controller
+     * @param {Express} app 
+     */
     static rotas(app){
+        /**
+         * Rota para buscar todos os usuários
+         */
         app.get("/usuarios", (req, res)=>{
             res.status(200).json({"usuario":"José das Couves"})
         })
 
+        /**
+         * Rota apra inserir um novo usuário
+         */
         app.post("/usuarios", (req, res)=>{
             const body = req.body
             console.log(body)
