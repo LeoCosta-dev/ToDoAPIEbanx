a0eceb33ebb9a74eac3c8be17e1dfe0f2717ec7e
Leonardo da Costa da Silva <leonardo.costa.silva@g.globo>
2023-09-04 10:44:58 -0300

    doc: inserção de comentários na model usuarios

--- src/models/UsuariosModel.js
+++ src/models/UsuariosModel.js
@@ -2,6 +2,12 @@
  * Objeto modelo para usuários
  */
 class UsuariosModel{
+    /**
+     * Construtor do objeto modelo para usuários
+     * @param {string} nome 
+     * @param {string} email 
+     * @param {string} telefone 
+     */
     constructor(nome, email, telefone){
         this.nome = nome
         this.email = email
